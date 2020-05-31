cb7233b2cea7d396d3ad8d73b1f183b0c6f94be5
Moutasem Akkad <39648266+moutasemalakkad@users.noreply.github.com>
2020-05-31 11:55:39 -0400

    added producer

---
+++ kafka/producer.py
@@ -0,0 +1,22 @@
+from time import sleep
+from json import dumps
+from kafka import KafkaProducer
+
+
+
+
+producer = KafkaProducer(bootstrap_servers=bootstrap_server   #to be added
+                         value_serializer=lambda x:
+                         #searlize object to send to kafkas
+                         dumps(x).encode('utf-8'))
+
+
+
+
+while True:
+	data = SchemaFaker().stream()
+
+
+# for data in SchemaFaker().stream():
+#     producer.send('meetup', value=data)   # add topic name
+#     sleep(1)
